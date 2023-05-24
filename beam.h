8a174a276245a5671e03815ebf74cfd9d8c195fd
YaoYuan20 <yaoyuan20@mails.tsinghua.edu.cn>
2023-05-24 16:08:57 +0800

    name changes from bar to beam

---
+++ beam.h
@@ -0,0 +1,40 @@
+/*****************************************************************************/
+/*  STAP++ : A C++ FEM code sharing the same input data file with STAP90     */
+/*     Computational Dynamics Laboratory                                     */
+/*     School of Aerospace Engineering, Tsinghua University                  */
+/*                                                                           */
+/*     Release 1.11, November 22, 2017                                       */
+/*                                                                           */
+/*     http://www.comdyn.cn/                                                 */
+/*     add beam element 2023/5/24 yy                                         */
+/*****************************************************************************/
+
+#pragma once
+
+#include "Element.h"
+
+using namespace std;
+
+//! Beam element class
+class CBeam : public CElement
+{
+public:
+
+//!	Constructor
+	CBeam();
+
+//!	Desconstructor
+	~CBeam();
+
+//!	Read element data from stream Input
+	virtual bool Read(ifstream& Input, CMaterial* MaterialSets, CNode* NodeList);
+
+//!	Write element data to stream
+	virtual void Write(COutputter& output);
+
+//!	Calculate element stiffness matrix
+	virtual void ElementStiffness(double* Matrix);
+
+//!	Calculate element stress
+	virtual void ElementStress(double* stress, double* Displacement);
+};
